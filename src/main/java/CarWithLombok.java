0ffe16085a00d4690b16a56ceb2bbe31b309430c
Sherran Kumaar <snagula1@stu.vistula.edu.pl>
2025-03-20 19:14:36 +0100

    git add . git commit -m "Add Lombok solution"

--- src/main/java/CarWithLombok.java
+++ src/main/java/CarWithLombok.java
@@ -1,12 +1,15 @@
 import lombok.Data;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-public class Car {
+@NoArgsConstructor
+public class CarWithLombok {
     private String model;
     private String brand;
     private int year;
-    private double price;
+    private int price;
+    private String color;
     private int quantity;
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        Car c1 = new Car("GLE", "Mercedes", 2023, 350_000, 2);
-        Car c2 = new Car("GLE", "Mercedes", 2023, 350_000, 2);
-        System.out.println(c1);
-        System.out.println(c2);
-        System.out.println("Are these objects equal? " + c1.equals(c2));
-        System.out.println("The hashcode of this object is: " + c1.hashCode());
-        System.out.println("The hashcode of this object is: " + c2.hashCode());
+        // 1) Create two different cars, as shown in your screenshot
+        CarWithLombok carWithLombok1 = new CarWithLombok("GLE", "Mercedes", 2020, 350000, "white", 5);
+        CarWithLombok carWithLombok2 = new CarWithLombok("GLC", "Mercedes", 2023, 200000, "silver", 3);
+
+        // 2) Print them (Lombok's toString())
+        System.out.println(carWithLombok1);
+        System.out.println(carWithLombok2);
+
+        // 3) Print the color of the first car
+        System.out.println(carWithLombok1.getColor());
+
+        // 4) Make the second car identical to the first (so equals() is true)
+        carWithLombok2.setModel("GLE");
+        carWithLombok2.setYear(2020);
+        carWithLombok2.setPrice(350000);
+        carWithLombok2.setColor("white");
+        carWithLombok2.setQuantity(5);
+
+        // 5) Now both are the same; print equality & matching hash codes
+        System.out.println("Are these objects equal? " + carWithLombok1.equals(carWithLombok2));
+        System.out.println("The HashCode of this object is: " + carWithLombok1.hashCode());
+        System.out.println("The HashCode of this object is: " + carWithLombok2.hashCode());
     }
 }
